f7b83ca17974429ffd72d9a2859546b50e91da67
donatto22 <ottanod22@gmail.com>
2024-02-12 16:10:01 -0500

    header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
         <div id="header">
             <div id="header-container">
                 <div id="header-content">
-                    <h1 data-aos="fade-right" data-aos-duration="1500">Tu comida FAVORITA en tu lugar preferido</h1>
-                    <p data-aos="fade-right" data-aos-duration="1700">A quien no le gusta comer desde la comodidad de su casa?</p>
+                    <div id='title-header' data-aos="fade-right" data-aos-duration="1500">Tu comida <text className="strong-text">FAVORITA</text> en tu lugar preferido</div>
+                    <div id='description-header' data-aos="fade-right" data-aos-duration="1700">A quien no le gusta comer desde la comodidad de su casa?</div>
 
                     <div id="header-button" data-aos="fade-up" data-aos-duration="2000" data-aos-once="true">
                         <button>
