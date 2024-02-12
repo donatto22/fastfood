20b06b186ec204fe3ce0750dbb352dd7b77aeb4e
donatto22 <ottanod22@gmail.com>
2024-02-12 17:11:50 -0500

    probando nueva funcionalidad

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,21 +1,45 @@
 import './menu.css'
 import './menu-responsive.css'
 
+import { MdOutlineMenu } from "react-icons/md"
+import { useEffect, useState } from 'react'
+import { isMobile } from 'react-device-detect'
+
 const Menu = () => {
-    return (
+
+    const [menu, setMenu] = useState(false)
+
+    useEffect(() => {
+        isMobile && setMenu(false)
+        !isMobile && setMenu(true)
+    }, [])
+    
+    return (    
         <div id="menu" data-aos="fade-down" data-aos-duration="1500" data-aos-once="true">
             <div id="menu-left">
                 <div id="logo">
                     <img src="/lockerglogo.png" alt="" />
                 </div>
-                FastFood
+
+                <div>
+                    FastFood
+                </div>
             </div>
 
-            <div id="menu-right">
-                <a href="#inicio">Inicio</a>
-                <a href="#food">Comida</a>
-                <a href="#coments">Comentarios</a>
+            <div id="burger-menu" onClick={() => { setMenu(!menu)}}>
+                <MdOutlineMenu />
             </div>
+
+            {
+                menu &&
+                (
+                    <div id="menu-right">
+                        <a href="#inicio">Inicio</a>
+                        <a href="#food">Comida</a>
+                        <a href="#coments">Comentarios</a>
+                    </div>
+                )
+            }
         </div>
     )
 }
